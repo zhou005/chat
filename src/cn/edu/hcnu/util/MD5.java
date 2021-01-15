9b518ee16f81a4c43ac05eee673ff3be04998cfb
zhoiu005 <2416383380@qq.com>
2021-01-15 17:33:28 +0800

    退出登录界面

--- src/cn/edu/hcnu/util/MD5.java
+++ src/cn/edu/hcnu/util/MD5.java
@@ -6,44 +6,31 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5 {
-    /**
-     * 利用MD5进行加密
-     *
-     * @param str 待加密的字符串
-     * @return 加密后的字符串
-     * @throws NoSuchAlgorithmException     没有这种产生消息摘要的算法
+    /**����MD5���м���
+     * @param str  �����ܵ��ַ���
+     * @return  ���ܺ���ַ���
+     * @throws NoSuchAlgorithmException  û�����ֲ�����ϢժҪ���㷨
      * @throws UnsupportedEncodingException
      */
     public static String encoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        //确定计算方法
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        //ȷ�����㷽��
+        MessageDigest md5= MessageDigest.getInstance("MD5");
         BASE64Encoder base64en = new BASE64Encoder();
-        //加密后的字符串
-        String newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
+        //���ܺ���ַ���
+        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
         return newstr;
     }
-
-
-    /**
-     * 判断用户密码是否正确
-     *
-     * @param newpasswd 用户输入的密码
-     * @param oldpasswd 数据库中存储的密码－－用户密码的摘要
+    /**�ж��û������Ƿ���ȷ
+     * @param newpasswd  �û����������
+     * @param oldpasswd  ���ݿ��д洢�����룭���û������ժҪ
      * @return
      * @throws NoSuchAlgorithmException
      * @throws UnsupportedEncodingException
      */
-    public static boolean checkpassword(String newpasswd, String oldpasswd) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        if (encoderByMd5(newpasswd).equals(oldpasswd))
+    public static boolean checkpassword(String newpasswd,String oldpasswd) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+        if(encoderByMd5(newpasswd).equals(oldpasswd))
             return true;
         else
             return false;
     }
 }
-/*//明文密码转换
-    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        System.out.println(encoderByMd5("ylx1234")); //输入需要转换的密码
-
-    }
-
- */
